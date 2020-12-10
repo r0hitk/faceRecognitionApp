cdbfd9624742d7bb21d9e24ce2648d2abbe62489
Rohit Kumar <rohit_kumarr@outlook.com>
2020-12-11 02:28:47 +0530

    Navigation bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Navigation from "./Components/Navigation/Navigation";
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation/>
-      {
-        /*<Logo/>
-      <ImageInputForm/>
-      <FaceRecognition/>
-      */}
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        {/*<Logo/>
+        <ImageInputForm/>
+        <FaceRecognition/>
+        */}
+      </div>
+    );
+  }
 }
 
 export default App;
