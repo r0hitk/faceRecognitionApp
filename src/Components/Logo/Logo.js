4cc0bbf9437c0c50639b3ba6aeb11ceeb611b694
Rohit Kumar <rohit_kumarr@outlook.com>
2020-12-11 03:33:27 +0530

    logo component

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,16 +1,17 @@
 import React from "react";
 import Tilt from "react-tilt";
 import "./Logo.css"
+import face from "./face.png";
 
 const Logo = () => {
   return (
-    <div className="ma4 mt0">
+    <div className="ma4 mt0 logo">
       <Tilt
         className="Tilt br2 shadow-2"
         options={{ max: 55 }}
         style={{ height: 100, width: 100 }}
       >
-        <div className="Tilt-inner"> 👽 </div>
+        <div className="Tilt-inner"><img src={face} alt="app logo"/> </div>
       </Tilt>
     </div>
   );
