9a740bf2ef0e1922631d4ae0688f70bff7916c56
Rohit Kumar <rohit_kumarr@outlook.com>
2020-12-13 23:58:28 +0530

    image link layout design

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -4,8 +4,8 @@ const ImageLinkForm = () => {
   return (<div>
 <p>{"This application detects faces in the pictures which you provide. Give it a go!"}</p>
 <div>
-    <input className="f4 pa2 w-70" type="text"/>
-    <button>Detect</button>
+    <input className="f5 pa2 w-70 center" type="text"/>
+    <button className="w-30 grow link f5 ph3 pv2 dib white bg-green">Detect</button>
 </div>
   </div>
   );
