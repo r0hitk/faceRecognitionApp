30f1b42c86c44c4f75ff9695a1b3d62669bd1363
Rohit Kumar <rohit_kumarr@outlook.com>
2020-12-14 00:43:34 +0530

    image link form style

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -1,13 +1,23 @@
 import React from "react";
+import "./ImageLinkForm.css"
 
 const ImageLinkForm = () => {
-  return (<div>
-<p>{"This application detects faces in the pictures which you provide. Give it a go!"}</p>
-<div>
-    <input className="f5 pa2 w-70 center" type="text"/>
-    <button className="w-30 grow link f5 ph3 pv2 dib white bg-green">Detect</button>
-</div>
-  </div>
+  return (
+    <div>
+      <p>
+        {
+          "This application detects faces in the pictures which you provide. Give it a go!"
+        }
+      </p>
+      <div className="center">
+        <div className="form pa4 br3 shadow-5 center">
+          <input className="f5 pa2 w-70 center" type="text" />
+          <button className="w-30 grow link f5 ph3 pv2 dib white bg-black">
+            Detect
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
