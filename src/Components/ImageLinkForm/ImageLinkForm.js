229e64e97e1247f71879b7c873c888bbca615561
Rohit Kumar <rohit_kumarr@outlook.com>
2020-12-13 23:48:24 +0530

    image link layout

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const ImageLinkForm = () => {
   return (<div>
-
+<p>{"This application detects faces in the pictures which you provide. Give it a go!"}</p>
+<div>
+    <input className="f4 pa2 w-70" type="text"/>
+    <button>Detect</button>
+</div>
   </div>
   );
 };
