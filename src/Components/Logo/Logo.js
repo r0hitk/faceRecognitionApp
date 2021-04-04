52d341343c66382f99b3b6b953d689343fbd6904
Rohit Kumar <59209227+r0hitk@users.noreply.github.com>
2021-04-04 20:47:31 +0530

    css changes 3

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -9,7 +9,7 @@ const Logo = () => {
       <Tilt
         className="Tilt br2 shadow-2"
         options={{ max: 55 }}
-        style={{ height: "100%", width: 100 }}
+        style={{ height: "100%", width: "100%" }}
       >
         <div className="Tilt-inner pa3" style={{ height: "100%" }}>
           <img
